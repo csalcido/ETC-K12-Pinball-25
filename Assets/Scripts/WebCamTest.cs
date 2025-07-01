0fd510fd0cb6c0c923eef3a255b57767649f9f65
c-salcido <71948615+csalcido@users.noreply.github.com>
2025-07-01 09:59:43 -0400

    Added Open CV packages, and two buff anims

---
+++ Assets/Scripts/WebCamTest.cs
@@ -0,0 +1,35 @@
+
+
+using UnityEngine;
+	using System.Collections;
+	using OpenCvSharp;
+    using OpenCvSharp.Demo;
+	using UnityEngine.UI;
+
+public class WebCamTest : WebCamera
+{
+    private Mat image;
+    protected override bool ProcessTexture(WebCamTexture input, ref Texture2D output)
+    {
+        Mat image = OpenCvSharp.Unity.TextureToMat(input);
+
+
+        //Convert image to grayscale
+        Mat imgGray = new Mat();
+        Cv2.CvtColor(image, imgGray, ColorConversionCodes.BGR2GRAY);
+        //image output
+        if (output == null)
+        {
+            output = OpenCvSharp.Unity.MatToTexture(imgGray);
+        }
+        else
+        {
+             OpenCvSharp.Unity.MatToTexture(imgGray, output);
+        }
+
+       
+        return true;
+
+       
+    }
+}
