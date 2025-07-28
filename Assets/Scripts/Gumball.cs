466aa6d49dfef8d987d989deb3018c84c96a1e1a
c-salcido <71948615+csalcido@users.noreply.github.com>
2025-07-28 03:07:44 -0400

    buff random spawn and animations set up, need to fix transforms

--- Assets/Scripts/Gumball.cs
+++ Assets/Scripts/Gumball.cs
@@ -7,11 +7,16 @@ public class Gumball : MonoBehaviour
     public GameObject[] spawnBuffs; //public array of prefabs to instantiate
     public Animator mainCameraAnimator;//Main camera animator
     public Animator leverAnimator;
+    public Transform[] spawnLocations;
+
+    public GameObject gumballDrop;
+
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    void Start()
     {
-        startGumballMachine();
+        StartCoroutine(startGumballMachine());
         
     }
 
@@ -19,30 +24,31 @@ public class Gumball : MonoBehaviour
     {
         yield return new WaitForSeconds(3f);
 
-        Debug.Log("gumballs started!");
-        
+
         //play lever animation
 
         //play lever sound
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < spawnLocations.Length; i++)
         {
-            //spawn 4 buffs at random
+            //randomize buff
+            var randomBuff = spawnBuffs[Random.Range(0, spawnBuffs.Length)];
+
+            //spawn buff at random
+            Instantiate(randomBuff, spawnLocations[i]);
+
+            //play animation of it coming out of gumball machine
+            Instantiate(randomBuff, gumballDrop.transform);
+
+            yield return new WaitForSeconds(2f);
         }
 
         mainCameraAnimator.SetBool("playBoardAnim", true);
 
         yield return null;
-    } 
-
-    void SpawnBuff()
-    {
-        //buff gets selected for animation
-
-        //buff gets instantiated on map
-
-        
     }
+
+   
 
     // Update is called once per frame
     void Update()
